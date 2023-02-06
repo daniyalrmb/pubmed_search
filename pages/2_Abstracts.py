defe0e581b843447927e460fbce035280140a5fa
daniyalrmb <77576726+daniyalrmb@users.noreply.github.com>
2023-02-07 01:22:52 +0500

    Update 2_Abstracts.py

--- pages/2_Abstracts.py
+++ pages/2_Abstracts.py
@@ -42,7 +42,7 @@ def getData(search):
     dff = dff.iloc[dff.isnull().sum(axis=1).mul(1).argsort()]
     #dff.columns =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o']
     #dff.fillna("-", inplace = True)
-    return dff
+    return len(pubids)
   
 @st.cache
 def convert_df(dff):
