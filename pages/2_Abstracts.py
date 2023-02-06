69da92bdfd3dccba3d6fd4e03d2a45fdff5defda
daniyalrmb <77576726+daniyalrmb@users.noreply.github.com>
2023-02-07 01:17:15 +0500

    Update 2_Abstracts.py

--- pages/2_Abstracts.py
+++ pages/2_Abstracts.py
@@ -70,4 +70,4 @@ st.download_button(
 time.sleep(1)
 
 
-AgGrid(p, height=500, fit_columns_on_grid_load=True, enable_enterprise_modules=True)
+#AgGrid(p, height=500, fit_columns_on_grid_load=True, enable_enterprise_modules=True)
